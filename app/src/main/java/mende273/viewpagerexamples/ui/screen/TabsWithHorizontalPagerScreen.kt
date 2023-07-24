3352cf757b435f289d111c1ddde479ce737e1448
mendejumrukovski <mende@code.livelike.com>
2023-07-24 15:07:44 +0200

    create TabsWithHorizontalPagerScreen.kt

---
+++ app/src/main/java/mende273/viewpagerexamples/ui/screen/TabsWithHorizontalPagerScreen.kt
@@ -0,0 +1,71 @@
+package mende273.viewpagerexamples.ui.screen
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
+import mende273.viewpagerexamples.ui.component.TabIndicator
+import mende273.viewpagerexamples.ui.component.TextComponent
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun TabsWithHorizontalPagerScreen() {
+    val pagerState = rememberPagerState()
+    val coroutineScope = rememberCoroutineScope()
+
+    val tabs = enumValues<Tab>()
+
+    Column {
+        TabRow(
+            containerColor = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.onBackground,
+            selectedTabIndex = pagerState.currentPage,
+            divider = { Divider(color = MaterialTheme.colorScheme.background) },
+            indicator = { TabIndicator(Modifier.tabIndicatorOffset(it[pagerState.currentPage])) }
+        ) {
+            tabs.forEachIndexed { index, item ->
+                Tab(
+                    selected = index == pagerState.currentPage,
+                    text = { Text(text = item.title) },
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.scrollToPage(index)
+                        }
+                    }
+                )
+            }
+        }
+
+        HorizontalPager(
+            pageCount = tabs.size,
+            state = pagerState,
+            userScrollEnabled = true
+        ) {
+            TabContents(tab = tabs[pagerState.currentPage])
+        }
+    }
+}
+
+@Composable
+fun TabContents(tab: Tab) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        TextComponent(text = "${tab.title} contents")
+    }
+}
+
+enum class Tab(val title: String) {
+    TAB_1("Tab 1"), TAB_2("Tab 2"), TAB_3("Tab 3")
+}
